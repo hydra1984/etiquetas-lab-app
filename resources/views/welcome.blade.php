8a16f57cd8f5b41530cc8860d43931c0637ab36d
hydra1984 <jordanalbarracin@gmail.com>
2024-11-20 10:03:08 -0300

    fix: se crea enlace a demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
       <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Free</h4>
+            <h4 class="my-0 font-weight-normal">Versión Demo</h4>
           </div>
           <div class="card-body">
             <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
@@ -50,7 +50,7 @@
               <li>Email support</li>
               <li>Help center access</li>
             </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+            <a href="https://nyc3.digitaloceanspaces.com/demoetiquetas/Mis%20Etiquetas%2028%20-%2010%20-24.rar" class="btn btn-lg btn-block btn-outline-primary">Descargar Demo</a>
           </div>
         </div>
         <div class="card mb-4 box-shadow">
