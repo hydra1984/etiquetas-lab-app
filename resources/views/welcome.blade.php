9e00216dba49f9b8566f582a253f3c7aa40f1416
hydra1984 <jordanalbarracin@gmail.com>
2024-11-20 12:20:48 -0300

    se modifica html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,16 +23,14 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
       <h5 class="my-0 mr-md-auto font-weight-normal">Hydra Software</h5>
       <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="#">Features</a>
-        <a class="p-2 text-dark" href="#">Enterprise</a>
-        <a class="p-2 text-dark" href="#">Support</a>
-        <a class="p-2 text-dark" href="#">Pricing</a>
+        <a class="p-2 text-dark" href="#">Soporte</a>
+        <a class="p-2 text-dark" href="#">Instructivos</a>
       </nav>
-      <a class="btn btn-outline-primary" href="#">Sign up</a>
+      <!--<a class="btn btn-outline-primary" href="#">Sign up</a>-->
     </div>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Pricing</h1>
+      <h1 class="display-4">Precios</h1>
       <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
     </div>
 
@@ -43,44 +41,44 @@
             <h4 class="my-0 font-weight-normal">Versión Demo</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/Trial</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
               <li>10 users included</li>
               <li>2 GB of storage</li>
               <li>Email support</li>
               <li>Help center access</li>
             </ul>
-            <a href="https://nyc3.digitaloceanspaces.com/demoetiquetas/Mis%20Etiquetas%2028%20-%2010%20-24.rar" class="btn btn-lg btn-block btn-outline-primary">Descargar Demo</a>
+            <a href="https://nyc3.digitaloceanspaces.com/demoetiquetas/Mis%20Etiquetas%2028%20-%2010%20-24.rar" class="btn btn-lg btn-block btn-info">Descargar Demo</a>
           </div>
         </div>
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Pro</h4>
+            <h4 class="my-0 font-weight-normal">Version Full</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">$ 17.500<small class="text-muted"></small></h1>
             <ul class="list-unstyled mt-3 mb-4">
               <li>20 users included</li>
               <li>10 GB of storage</li>
               <li>Priority email support</li>
               <li>Help center access</li>
             </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Obtener</button>
           </div>
         </div>
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Enterprise</h4>
+            <h4 class="my-0 font-weight-normal">Base de Datos</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">$ 8.500<small class="text-muted"></small></h1>
             <ul class="list-unstyled mt-3 mb-4">
               <li>30 users included</li>
               <li>15 GB of storage</li>
               <li>Phone and email support</li>
               <li>Help center access</li>
             </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Comprar</button>
           </div>
         </div>
       </div>
